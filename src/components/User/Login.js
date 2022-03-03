38534e73cc49ac7df59063c771862344eb872efa
DevEnock <enockomondi305@gmail.com>
2022-03-03 23:42:03 +0300

    adding the login form

--- src/components/User/Login.js
+++ src/components/User/Login.js
@@ -1,29 +1,75 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+  constructor(props){
+    super(props)
+    this.state ={
+      username: '',
+      email: '',
+      password: '',
+      profession: ''
+    }
+  }
+
+  // handle username input
+  handleUsernameInputChange = (event) =>{
+    this.setState({
+      username: event.target.value
+    })
+  }
+
+  // handle email input
+  handleEmailInputChange = (event) =>{
+    this.setState({
+      email: event.target.value
+    })
+  }
+
+  // handle password input
+  handlePasswordInputChange = (event) =>{
+    this.setState({
+      password: event.target.value
+    })
+  }
+
+  // handle profession input
+  handleProfessionInputChange = (event) =>{
+    this.setState({
+      profession: event.target.value
+    })
+  }
+
+
+  // handle form submission
+  onFormSubmission = (event) =>{
+    
+    event.preventDefault();
+
+    const newUser = {
+      
+    }
+  }
+
+
   render() {
     return (
       <div className='login'>
-      <form>
+      <form onSubmit={this.onFormSubmission}>
         <div class="mb-3">
           <label for="username" class="form-label">Username</label>
-          <input type="username" class="form-control" id="username" />
+          <input type="username" value={this.state.username} onChange={this.handleUsernameInputChange} class="form-control" id="username" />
         </div>
         <div class="mb-3">
           <label for="email" class="form-label">Email</label>
-          <input type="email" class="form-control" id="email" />
+          <input type="email" value={this.state.email} onChange={this.handleEmailInputChange} class="form-control" id="email" />
         </div>
         <div class="mb-3">
           <label for="password" class="form-label">Password</label>
-          <input type="password" class="form-control" id="password" />
+          <input type="password" value={this.state.password} onChange={this.handlePasswordInputChange} class="form-control" id="password" />
         </div>
         <div class="mb-3">
           <label for="profession" class="form-label">Profession</label>
-          <input type="profession" class="form-control" id="profession" />
-        </div>
-        <div class="mb-3 form-check">
-          <input type="checkbox" class="form-check-input" id="check" />
-          <label class="form-check-label" for="check">Check me out</label>
+          <input type="profession" value={this.state.profession} onChange={this.handleProfessionInputChange} class="form-control" id="profession" />
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
       </form>
