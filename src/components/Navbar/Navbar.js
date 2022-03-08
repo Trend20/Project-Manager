af7e7d5a6a70f99c64345fb94fac9e9216e5003e
DevEnock <enockomondi305@gmail.com>
2022-03-08 12:13:46 +0300

    adding register page

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,22 +4,22 @@ import { Link } from 'react-router-dom'
 class Navbar extends Component {
   render() {
     return (
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark w-100">
         <div class="container-fluid">
-          <Link class="navbar-brand" to="/">Navbar</Link>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <Link class="navbar-brand" to="/">Manager</Link>
+          {/* <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
-          </button>
+          </button> */}
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav">
             <li class="nav-item">
-              <Link class="nav-link" to="/">Login</Link>
+              <Link class="nav-link active" to="/">Login</Link>
             </li>
             <li class="nav-item">
-              <Link class="nav-link active" aria-current="page" to="/">Projects</Link>
+              <Link class="nav-link active" aria-current="page" to="/projects">Projects</Link>
             </li>
             <li class="nav-item">
-              <Link class="nav-link" to="/logout">Logout</Link>
+              <Link class="nav-link active" to="/login">Logout</Link>
             </li>
           </ul>
           </div>
