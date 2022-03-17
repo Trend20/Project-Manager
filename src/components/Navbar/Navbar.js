ef356907f6c6bbfdb8ddaef4a5339319c3d80086
DevEnock <enockomondi305@gmail.com>
2022-03-17 15:44:30 +0300

    adding the logo

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
+import './Navbar.css'
 
 class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark w-100">
-        <div className="container-fluid">
-          <Link className="navbar-brand ml-5" to="">Manager</Link>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link className="nav-link active" to="/">Register</Link>
-            </li>
+      <nav className="w-100" id="nav">
+        <div  id='header'>
+         <div className="logo">
+          <img src="./logo2.png" alt="logo" />
+         </div>
+        <div id="navbarNav">
+          <ul className="nav-container">
             <li className="nav-item">
               <Link className="nav-link active" aria-current="page" to="/projects">Projects</Link>
             </li>
+            <li className="nav-item">
+              <Link className="nav-link active" to="/">Register</Link>
+            </li>
+            
             <li className="nav-item">
               <Link className="nav-link active" to="/login">Logout</Link>
             </li>
