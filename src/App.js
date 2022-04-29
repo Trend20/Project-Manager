ac39dccb285417733da82ac85ca4b0b148502ee6
DevEnock <enockomondi305@gmail.com>
2022-04-29 12:43:19 +0300

    home page layout

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import Project from './components/Project/Project';
 import Login from './components/User/Login';
 import Register from './components/User/Register';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar/Navbar';
-import Hero from './components/Hero/Hero';
+import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
 
 function App() {
   return (
    <Router>
-     <Navbar />
-      <div className="container mt-5">
-       <Hero /> 
+      <div className="container mt-5"> 
       <Routes>
+         <Route path="/" element={<Home />} />
          <Route path="/register" element={<Register />} />
          <Route path="/login" element={<Login />} />
          <Route path="/projects" element={<Project />} />
       </Routes>
+      <Footer />
     </div>
    </Router>
   );
